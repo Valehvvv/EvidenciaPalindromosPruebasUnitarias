fc06efbaf4c8afd593163faff1448845d9015f4b
mjosepc <142420543+mjosepc@users.noreply.github.com>
2023-09-12 21:13:59 -0300

    Add files via upload
    se le agrego la modificacion para que arroje la excepcion para los numeros

---
+++ Main.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean esPalindromo(String cadena) {
+        cadena = cadena.toLowerCase();
+        String palabraInicial = cadena;
+
+        String[] caracteres = palabraInicial.split("");
+        StringBuilder reverso = new StringBuilder();
+        for (int i = caracteres.length - 1; i >= 0; i--) {
+            reverso.append(caracteres[i]);
+        }
+
+        String palabraFinal = reverso.toString();
+
+        return palabraInicial.equals(palabraFinal);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese una cadena de texto: ");
+        String cadena = scanner.nextLine();
+        scanner.close();
+
+        if (contieneNumeros(cadena)) {
+            System.err.println("Error: La cadena contiene números y no puede ser un palíndromo.");
+            System.exit(1); // Salir con código de error
+        }
+
+        boolean esPalindromo = esPalindromo(cadena);
+
+        if (esPalindromo) {
+            System.out.println(" es un palíndromo.");
+        } else {
+            System.out.println(" no es un palíndromo.");
+        }
+    }
+    public static boolean contieneNumeros(String cadena) {
+        return cadena.matches(".*\\d.*");
+    }
+}
